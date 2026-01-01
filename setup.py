d08ee609540949b9046393c840f928f4eb66f1f2
JA-GK-00 <aaf6@o2.pl>
2026-01-01 02:17:33 +0100

    saved

--- setup.py
+++ setup.py
@@ -5,15 +5,30 @@ from pathlib import Path
 import numpy as np
 from setuptools import Extension, setup, find_packages
 
+from mypyc.build import mypycify
+
 root = Path(__file__).parent
 
-ext = Extension(
+cext = Extension(
     "fastfractal._cext",
-    sources=[str("fastfractal" + "/_cext.c")],
+    sources=["fastfractal/_cext.c"],
     include_dirs=[np.get_include()],
 )
 
+mypyc_targets = [
+    "fastfractal/utils/entropy.py",
+]
+
+ext_modules = [
+    cext,
+    *mypycify(
+        mypyc_targets,
+        debug_level="0",
+    ),
+]
+
 setup(
     packages=find_packages(where="."),
-    ext_modules=[ext],
+    ext_modules=cext,
+    zip_safe=False,
 )
